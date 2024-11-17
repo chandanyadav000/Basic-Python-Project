48252232971199fea0ec3c0894e79b4d0d809ea5
Chandan Yadav <chandanmailbox00@gmail.com>
2024-11-17 08:01:10 +0545

    added pseudo code

---
+++ atm_machine.py
@@ -0,0 +1,32 @@
+'''
+Enter your pin
+
+
+If incorrect
+
+repeat
+again repeat
+repeat again
+
+print(sorry, your card has been blocked due to numeroues attempt)
+
+
+if correct
+show options
+
+if chosen withdraw option
+take input to enter amount and subtract it from main balance
+
+if chosen depost option
+take input to enter amount and add it to main balance
+
+if chosen check balance
+print balance
+
+
+
+
+
+
+
+'''
